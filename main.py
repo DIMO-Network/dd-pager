33aec84b02b81fbd3996bf34ae7e9f402d4cd3f1
Dylan Moreland <dylan@dimo.zone>
2024-10-21 14:05:13 -0400

    Explode DD

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 import sys
+import typing
 
 vehicle_query = """
 query PageDefinitionVehicles($deviceDefinitionId: String!, $after: String) {
@@ -15,7 +16,7 @@ query PageDefinitionVehicles($deviceDefinitionId: String!, $after: String) {
 }
 """
 
-definition_query = """
+definitions_query = """
 query PageDefinitions($after: String) {
     vehicles(first: 100, after: $after) {
         pageInfo {
@@ -27,6 +28,18 @@ query PageDefinitions($after: String) {
                 id
             }
         }
+    }
+}
+"""
+
+definition_query = """
+query ExplodeDefinition($id: String!) {
+    deviceDefinition(by: {id: $id}) {
+        manufacturer {
+            name
+        }
+        model
+        year
     }
 }
 """
@@ -68,7 +81,7 @@ def find_used_definitions() -> list[str]:
         resp = requests.post(
             "https://identity-api.dimo.zone/query",
             json={
-                "query": definition_query,
+                "query": definitions_query,
                 "variables": {"after": after},
             },
         )
@@ -89,9 +102,23 @@ def find_used_definitions() -> list[str]:
     return list(definition_ids)
 
 
+def explode_dd(definition_id: str) -> typing.Tuple[str, str, int]:
+    resp = requests.post(
+        "https://identity-api.dimo.zone/query",
+        json={
+            "query": definition_query,
+            "variables": {"id": definition_id},
+        },
+    )
+
+    inner = resp.json()["data"]["deviceDefinition"]
+
+    return (inner["manufacturer"]["name"], inner["model"], inner["year"])
+
+
 if __name__ == "__main__":
     if len(sys.argv) == 1:
-        print('Provide a sub-command, either "dd-vehicles" or "dds"')
+        print('Provide a sub-command, either "dd-vehicles", "explode-dd", or "list-dds"')
         sys.exit(1)
 
     sub_command = sys.argv[1]
@@ -102,9 +129,15 @@ if __name__ == "__main__":
         definition_id = sys.argv[2]
         for i in find_vehicles_with_definition(definition_id):
             print(i)
-    elif sub_command == "dds":
+    elif sub_command == "list-dds":
         for s in find_used_definitions():
             print(s)
+    elif sub_command == "explode-dd":
+        if len(sys.argv) < 3:
+            print("Provide device definition id")
+            sys.exit(1)
+        make, model, year = explode_dd(sys.argv[2])
+        print("Make: {}, Model: {}, Year: {}".format(make, model, year))
     else:
         print("Unrecognized sub-command {}".format(sub_command))
         sys.exit(1)
